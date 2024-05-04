eef575c8d2b726ab3642033c1e396c83e4386036
Alexander Hunt <alexhunt215@gmail.com>
2024-05-04 13:35:58 -0400

    completed Client-app

--- client-app/src/components/Footer/Footer.tsx
+++ client-app/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ interface IFooterProps {}
 
 const Footer: React.FunctionComponent<IFooterProps> = () => {
 	return (
-		<div className='bg-slate-900 text-white text-base text-center'>
+		<div className='bg-slate-900 text-white text-base text-center py-5'>
 			Copyright &#169; URLShortner | Alex H
 		</div>
 	);
